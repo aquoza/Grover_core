971f2a43136cf42c4931647d3340b9c483522d61
Sathvik Kanna <k.sathvik2003@gmail.com>
2023-12-21 14:06:52 +0530

    change

--- CORE_V2/CORE_V2.ino
+++ CORE_V2/CORE_V2.ino
@@ -2,44 +2,53 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <params.h>
+#include "params.h"
 
 /**
-0 - targetgps_NHIGH
-1 - targetgps_NLOW
-2 - targetgps_EHIGH
-3 - targetgps_ELOW
-4 - currentgps_NHIGH
-5 - currentgps_NLOW
-6 - currentgps_EHIGH
-7 - currentgps_ELOW
-8 - currentheadingHIGH
-9 - currentheadingLOW
-**/
-byte dataget[10]; //input data from RPI
+  0 - targetgps_NHIGH
+  1 - targetgps_NLOW
+  2 - targetgps_EHIGH
+  3 - targetgps_ELOW
+  4 - currentgps_NHIGH
+  5 - currentgps_NLOW
+  6 - currentgps_EHIGH
+  7 - currentgps_ELOW
+  8 - currentheadingHIGH
+  9 - currentheadingLOW
+ **/
+byte dataGet[6]; //input data from RPI
+double target_GPS[2];
+double current_heading;
+int count = 0;
+double moving_average[3] = {0,0,0};
+double head[3] = {0,0,0};
+double tail[3] = {MOVNG_AVERAGE_SIZE - 1, MOVNG_AVERAGE_SIZE - 1, MOVNG_AVERAGE_SIZE - 1};
+double N_array[MOVNG_AVERAGE_SIZE];
+double E_array[MOVNG_AVERAGE_SIZE];
+double yaw_array[MOVNG_AVERAGE_SIZE];
 
-void requestData()
+void recieveData(int byteCount)
 {
-	Wire.requestFrom(0x08, 10);
-	for (int i = 0; i < 10; i++) {
-		dataget[i] = Wire.read(); 
+	while(Wire.available()){
+		for (int i = 0; i < 6; i++) {
+			dataGet[i] = Wire.read(); 
+		}
+		target_GPS[0] = (dataGet[0] << 8 | dataGet[1])/10;
+		target_GPS[1] = (dataGet[2] << 8 | dataGet[3])/10;
+		current_heading = (dataGet[4] << 8 | dataGet[5])/10;
 	}
-	target_GPS[0] = (dataGet[0] << 8 | dataGet[1])/10;
-	target_GPS[1] = (dataGet[2] << 8 | dataGet[3])/10;
-	current_GPS[0] = (dataGet[4] << 8 | dataGet[5])/10;
-	current_GPS[1] = (dataGet[6] << 8 | dataGet[7])/10;
-	current_heading = (dataGet[8] << 8 | dataGet[9])/10;
 }
+
 /*
-Manual RC control
+   Manual RC control
 INPUTS :
-	- mode : rover operation mode
-	- throttle : raw throttle reading
-	- steering : raw steering reading
+- mode : rover operation mode
+- throttle : raw throttle reading
+- steering : raw steering reading
 
 OUTPUTS :
-	- array with four STM parameters
-*/
+- array with four STM parameters
+ */
 uint8_t* Manual(uint8_t mode, int throttle, int steering){
 
 	uint8_t output[] = {1 , 0 , 90 , 0};
@@ -61,20 +70,20 @@ uint8_t* Manual(uint8_t mode, int throttle, int steering){
 			if(80 < modifier && modifier < 100){modifier = 90;}
 			break;
 
-		//Ackermann
+			//Ackermann
 		case 2 : 
 			modifier = map(steering, 950, 2050, 0 , 255);
 			if(126 - ERR_ACKERMANN < modifier && modifier < 126 + ERR_ACKERMANN){modifier = 127;}
 			break;
 
-		//UTurn
+			//UTurn
 		case 3 : 
 			if(direction == 2){ modifier = 0; }
 			else if(speed == 0){ modifier = 1; }
 			else{modifier = 2;}
 			break;
-	
-		//Idle
+
+			//Idle
 		case 0 :
 			mode = 1;
 			speed = 0;
@@ -94,10 +103,18 @@ uint8_t* Manual(uint8_t mode, int throttle, int steering){
 	output[1] = speed;
 	output[2] = modifier;
 	output[3] = direction;
-	
+
 	return output;
 }
 
+double calculate_heading(double target_GPS){
+	if(target_GPS[1] >= 0){
+		return 90 - atan(target_GPS[0]/target_GPS[1])*57.2957795131;
+	}
+	else{
+		return -90 - atan(target_GPS[0]/target_GPS[1])*57.2957795131;
+	}
+}
 
 uint8_t* correct_heading(double current_heading, double target_heading){
 
@@ -109,51 +126,61 @@ uint8_t* correct_heading(double current_heading, double target_heading){
 	}
 }
 
-uint8_t* Autonomous (vector<double>target_GPS, vector<double> current_GPS, double current_heading){
+uint8_t* Autonomous (double target_GPS[2],  double current_heading){
 
-	//Implement queue for moving average
-	queue <vector<double>> avg_Q;
-	vector<double> moving_average = 0;
-	if(moving_average.size() < MOVNG_AVERAGE_SIZE){
-		avg_Q.push(current_GPS);
-		moving_average[0] += current_GPS[0];
-		moving_average[1] += current_GPS[1];  
+	// Implement queue for moving average
+	if(count < MOVNG_AVERAGE_SIZE){
+		N_array[count] = target_GPS[0];
+		E_array[count] = target_GPS[1];
+		yaw_array[count] = current_heading;
+
+		moving_average[0] += target_GPS[0];
+		moving_average[1] += target_GPS[1];
+		moving_average[2] += current_heading;
+		count ++;
 		return IDLE;
 	}
-	moving_average[0] -= avg_Q.front()[0];
-	moving_average[1] -= avg_Q.front()[1];
-	avg_Q.pop();
-	avg_Q.push(current_GPS);
-	moving_average[0] += avg_Q.end()[0];
-	moving_average[1] += avg_Q.end()[1];
+		
+	moving_average[0] += target_GPS[0] - N_array[head[0]];
+	moving_average[1] += target_GPS[1] - E_array[head[0]];
+	moving_average[2] += current_heading - yaw_array[head[2]];
 
-	current_GPS[0] = moving_average[0]/MOVNG_AVERAGE_SIZE;
-	current_GPS[1] = moving_average[1]/MOVNG_AVERAGE_SIZE;
+	for(int i = 0; i < 3; i++){
+		head[i] = (head[i] + 1)%MOVNG_AVERAGE_SIZE;
+		tail[i] = (tail[i] + 1)%MOVNG_AVERAGE_SIZE;
+	}
+
+	N_array[tail[0]] = target_GPS[0];
+	E_array[tail[1]] = target_GPS[1];
+	yaw_array[tail[2]] = current_heading;
+
+	target_GPS[0] = moving_average[0]/MOVNG_AVERAGE_SIZE;
+	target_GPS[1] = moving_average[1]/MOVNG_AVERAGE_SIZE;
+	current_heading = moving_average[2]/MOVNG_AVERAGE_SIZE;
 
 	//convert target heading to degrees
-	double target_heading = 90 - atan((target_GPS[0] - current_GPS[0])/
-										(target_GPS[1] - current_GPS[1]))*57.2957795131;
+	double target_heading = calculate_heading(target_GPS);
 
 	//Is reached
-	if(target_GPS[0] - 2 < current_GPS[0] && current_GPS[1] < target_GPS[0] + 2 &&
-		target_GPS[1] - 2 < current_GPS[1] && current_GPS[1] < target_GPS[1] + 2){
+	if(-2 < target_GPS[0] && target_GPS[0] < 2 && -2 < target_GPS[1] && target_GPS[1] < 2){
 		return IDLE;
 	}
 	//correct heading if its off
 	if(!(target_heading - ERR_HEADING < current_heading && current_heading < target_heading + ERR_HEADING)){
-		return correct_heading;
+		return correct_heading(current_heading, target_heading);
 	}
-	
+
 
 	return FORWARD;
 
 }
 
 void setup() {
-	Wire.begin();
-	Wire.onReceive(receiveData);
-
 	delay(5000);
+
+	Wire.begin(0x08);
+	Wire.onReceive(recieveData);
+
 	Serial.begin(baudRate);
 
 	pinMode(CH1, INPUT);
@@ -182,18 +209,13 @@ void loop(){
 	else { mode = 3; }
 
 	//Autonomous or Manual depending on SwitchC
+	int arraySize = 4;
 	if(SwitchC > 1500){
-		requestData();
-		command = Autonomous(target_GPS, current_GPS, current_heading);
+		Serial.write(Autonomous(target_GPS, current_GPS, current_heading), sizeof(int) * arraySize);
 	}
 	else {
-		command = Manual(mode, throttle, steering);
+		Serial.write(Manual(mode, throttle, steering), sizeof(int) * arraySize);
+		delay(100);  // Adjust the delay based on your requirements
 	}
-
-	// Send the array over UART
-	int arraySize = 4;
-	Serial.write(command, sizeof(int) * arraySize);
-
-	delay(100);  // Adjust the delay based on your requirements
 
 }
--- pixhawk/gps_interface.py
+++ pixhawk/gps_interface.py
@@ -1,6 +1,17 @@
+import sys
+import smbus2 as smbus#,smbus2
 import time
 from pymavlink import mavutil
+import navpy
 
+LATITUDE_TARGET = 0
+LONGITUDE_TARGET = 0
+YAW_NORTH_BIAS = 0
+
+I2Cbus = smbus.SMbus(1)
+slaveAddress = 0x08
+
+data = [0]*6
 # Connect to Pixhawk over the appropriate connection (e.g., '/dev/ttyUSB0' or 'udpin:0.0.0.0:14550')
 connection = mavutil.mavlink_connection('/dev/ttyACM0')
 print(connection)
@@ -12,21 +23,35 @@ while True:
     latitude = gps_msg.lat / 1e7  # Convert from degrees * 1e7 to degrees
     longitude = gps_msg.lon / 1e7  # Convert from degrees * 1e7 to degrees
     altitude = gps_msg.alt / 1000  # Convert from millimeters to meters
-    
+   
+    NED_target = navpy.lla2ned(LATITUDE_TARGET, LONGITUDE_TARGET, 0, latitude, longitude, 0)
     # Wait for an ATTITUDE message (yaw data)
     attitude_msg = connection.recv_match(type='ATTITUDE', blocking=True)
-    print('hi')
+    # print('hi')
     # Extract yaw (heading vector) in radians
     yaw = attitude_msg.yaw
 
     # Convert yaw from radians to degrees
-    yaw_degrees = yaw * 180 / 3.14159265359
-
+    yaw_degrees = yaw * 180 / 3.14159265359 + 180 + YAW_NORTH_BIAS
     # Wait for a VFR_HUD message (velocity data)
     velocity_msg = connection.recv_match(type='VFR_HUD', blocking=True)
     
     # Extract velocity data
     ground_speed = velocity_msg.groundspeed  # Ground speed in meters per second
+
+    NED_target[0] = (NED_target[0]*10)%1
+    NED_target[1] = (NED_target[1]*10)%1
+    yaw_degrees = (yaw_degrees*10)%1
+    data[0] = NED_target[0]>>8
+    data[1] = NED_target[0] % 256
+    data[2] = NED_target[1]>>8
+    data[3] = NED_target[1] % 256
+    data[4] = yaw_degrees >>8
+    data[5] = yaw_degrees % 256
+    
+    with smbus.SMBus(1) as I2Cbus:
+        I2Cbus.write_i2c_block_data(slaveAddress, 0x00, data)
+        time.sleep(0.1)
 
     print(f"Latitude: {latitude}, Longitude: {longitude}, Altitude: {altitude} m, Yaw: {yaw_degrees} degrees, Ground Speed: {ground_speed} m/s")
    # print(f" Yaw: {yaw_degrees}")
